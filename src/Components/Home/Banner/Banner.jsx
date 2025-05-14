493e9af27706d9a3965078c98b3ded8841edd6e8
shossen1999 <shossen1999@gmail.com>
2025-05-14 12:49:20 +0600

    resume updated

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -6,7 +6,7 @@ import banner from '../../../assets/finish.png';
 import { GoDownload } from "react-icons/go";
 import { FaChevronCircleRight } from "react-icons/fa";
 
-import resume from "../../../assets/resume.pdf"
+import resume from "../../../assets/resume1.pdf"
 
 const Banner = () => {
   
