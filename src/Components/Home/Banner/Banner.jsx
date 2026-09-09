be90a4f8d386310a8ed2d53e963da51c343eea72
shossen1999 <shossen1999@gmail.com>
2026-09-09 13:33:19 +0600

    CV Updation

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -6,7 +6,7 @@ import banner from '../../../assets/ban7.png';
 import { GoDownload } from "react-icons/go";
 import { FaChevronCircleRight } from "react-icons/fa";
 
-import resume from "../../../assets/Soheb_Hossen_IT_Executive.pdf"
+import resume from "../../../assets/Soheb Hossen_Software Developer.pdf"
 
 const Banner = () => {
 
@@ -23,10 +23,9 @@ const Banner = () => {
                     <h1 className='text-2xl md:text-3xl lg:text-5xl font-bold pb-2 text-black'>
                         I'm <span className='text-[#548AA3]'>Md. Soheb Hossen</span>
                     </h1>
-                    <p className='text-[#548AA3] text-[16px] md:text-xl pb-4 font-medium'>IT Executive | ERP & Business Applications</p>
+                    <p className='text-[#548AA3] text-[16px] md:text-xl pb-4 font-medium'>Full Stack Web Developer</p>
                     <p className='pb-4 text-white text-[10px] md:text-[16px]'>
-                        Computer Science & Engineering graduate with hands-on experience in ERP configuration, business process mapping, IT support, system testing, and user assistance. Currently working as an Executive in IT & Support Services at Process IT Limited, supporting Odoo Business Management Software and client business operations. Experienced in system configuration, troubleshooting, user support, business workflow analysis, and ERP modules including Sales, Purchase, Inventory, Accounting, and POS,  with additional full-stack development experience using the MERN stack.
-                    </p>
+                         Computer Science & Engineering graduate with hands-on experience in full-stack web development using React, Node.js, Express.js, and MongoDB. Experienced in developing responsive web applications, RESTful APIs, authentication systems, role-based access control, and database-driven applications. Currently working in IT & Support Services with practical experience in business software, system configuration, troubleshooting, and ERP workflows.
 
                     <div className='lg:flex gap-8'>
                         <a href={resume} download="Resume">
